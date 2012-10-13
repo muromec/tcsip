3b57e621c8caaf342cee4e88590f9018b06ac826
Ilya Petrov <ilya.muromec@gmail.com>
2012-10-13 08:08:15 +0700

    update project setup

---
+++ libresip-osx/libresip_osx.m
@@ -0,0 +1,13 @@
+//
+//  libresip_osx.m
+//  libresip-osx
+//
+//  Created by Ilya Petrov on 10/13/12.
+//  Copyright (c) 2012 enodev. All rights reserved.
+//
+
+#import "libresip_osx.h"
+
+@implementation libresip_osx
+
+@end
