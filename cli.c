abab913b9230d2ea98d6aadd23079cf88d5ba6aa
Ilya Petrov <ilya.muromec@gmail.com>
2013-04-02 17:36:50 +0700

    use tcsip in cli

--- cli.c
+++ cli.c
@@ -1,4 +1,5 @@
 #include "re.h"
+#include "tcsip.h"
 #include "tcsipuser.h"
 #include "tcsipreg.h"
 #include "tcsipcall.h"
@@ -11,34 +12,30 @@
 
 #define USER_AGENT "Linux re"
 
-struct uac {
-    struct sip *sip;
-    struct sa laddr;
-    struct sipsess_sock *sock;
-    struct dnsc *dnsc;
-    struct tls *tls;
-    struct sa nsv[16];
-    uint32_t nsc;
+struct cli_app {
+    struct tcsip *sip;
+    void *current_call;
+    struct sip_handlers *handlers;
     struct sip_addr *local;
 };
 
-static void *current_call = NULL;
+void *glob_app;
 static int exit_after = 0, auto_accept = 0;
-void call(struct uac *uac, char *name);
+void call(struct cli_app *app, char *name);
 
 static void eat() {
     printf("\x1b[1K\n\x1b[1A");
 }
 
-static void prompt(struct uac *uac) {
+static void prompt(struct cli_app *app) {
     eat();
-    re_printf("texr %r> ", &uac->local->uri.user);
+    re_printf("texr %r> ", &app->local->uri.user);
     fflush(stdout);
 }
 
 static void cmd_handler(int flags, void *arg)
 {
-    struct uac *uac = arg;
+    struct cli_app *app = arg;
     int len;
     char buf[100];
     memset(buf, 0, 100);
@@ -47,22 +44,22 @@ static void cmd_handler(int flags, void *arg)
 
     if(len > 5 && !strncmp(buf, "call ", 5) ) {
 	buf[len-1] = '\0';
-	call(uac, buf+5);
+	call(app, buf+5);
 	goto exit;
     }
     if(len > 4 && !strncmp(buf, "hang", 4)) {
-        if(current_call)
-            tcsipcall_control(current_call, CALL_BYE);
-        current_call = NULL;
+        if(app->current_call)
+            tcsipcall_control(app->current_call, CALL_BYE);
+        app->current_call = NULL;
 	goto exit;
     }
     if(len == 2 && !strncmp(buf, "A\n", 2)) {
-	if(current_call)
-            tcsipcall_control(current_call, CALL_ACCEPT);
+	if(app->current_call)
+            tcsipcall_control(app->current_call, CALL_ACCEPT);
 	goto exit;
     }
 exit:
-    prompt(uac);
+    prompt(app);
 }
 
 static void exit_handler(void *arg)
@@ -72,14 +69,17 @@ static void exit_handler(void *arg)
 
 static void signal_handler(int sig)
 {
-    if(current_call) tcsipcall_control(current_call, CALL_BYE);
-    current_call = NULL;
+    struct cli_app *app = glob_app;
+    if(app->current_call)
+        tcsipcall_control(app->current_call, CALL_BYE);
+    app->current_call = NULL;
     re_cancel();
 }
 
 void call_change(struct tcsipcall* call, void *arg)
 {
     int cstate;
+    struct cli_app *app = arg;
 
     tcsipcall_dirs(call, NULL, &cstate, NULL, NULL);
 
@@ -87,7 +87,7 @@ void call_change(struct tcsipcall* call, void *arg)
 	eat();
         re_printf("call ended\n");
         tcsipcall_remove(call); // wrong place for this
-	current_call = NULL;
+	app->current_call = NULL;
 	if(exit_after)
 	    re_cancel();
     }
@@ -110,127 +110,114 @@ void reg_change(enum reg_state state, void*arg) {
     prompt(arg);
 }
 
-static void connect_handler(const struct sip_msg *msg, void *arg)
+void call(struct cli_app *app, char *name)
 {
-    int err;
-    struct uac *uac = arg;
-    struct tcsipcall *call;
 
-    err = tcsipcall_alloc(&call, uac);
-    tcop_users((void*)call, uac->local, NULL);
-    tcsipcall_incomfing(call, msg);
-    tcsipcall_accept(call);
-    tcsipcall_handler(call, call_change, uac);
-    if(auto_accept)
-        tcsipcall_control(call, CALL_ACCEPT);
-    current_call = call;
-
-    eat();
-    if(msg->from.dname.l) {
-        re_printf("incoming call %r %r\n", &msg->from.dname, &msg->from.auri);
-    } else {
-        re_printf("incoming call %r\n", &msg->from.auri);
-    }
-    prompt(uac);
-}
-
-void call(struct uac *uac, char *name)
-{
     struct tcsipcall *call;
     struct sip_addr *dest;
     int err;
 
     err = sippuser_by_name(&dest, name);
-    err = tcsipcall_alloc(&call, uac); 
-    tcop_users((void*)call, uac->local, dest);
-    tcsipcall_out(call);
-    tcsipcall_handler(call, call_change, uac);
+    if(err) {
+        printf("failed to create user\n");
+        return;
+    }
 
-    current_call = call;
+    tcsip_start_call(app->sip, dest);
+    app->current_call = call;
 
     re_printf("dialing %r\n", &dest->auri);
 }
 
+void got_call(struct tcsipcall* call, void *arg) {
+    int cdir;
+    struct sip_addr *remote;
+    struct cli_app *app = arg;
+
+    tcsipcall_dirs(call, &cdir, NULL, NULL, NULL);
+
+    app->current_call = call;
+
+    tcop_lr((void*)call, NULL, &remote);
+    if(cdir==CALL_IN) {
+        eat();
+        re_printf("incoming call %r\n", &remote->auri);
+        prompt(arg);
+    }
+}
+
+void got_cert(int err, struct pl*name, void*arg) {
+    struct cli_app *app = arg;
+    struct sip_addr *local = tcsip_user(app->sip);
+    if(local)
+        app->local = mem_ref(local);
+
+    re_printf("Hello, %r <%r> !\n", name, &local->auri);
+}
+
+static struct sip_handlers cli_handlers = {
+    .call_h = got_call,
+    .call_ch = call_change,
+    .cert_h = got_cert,
+    .reg_h = reg_change,
+};
+
 int main(int argc, char *argv[]) {
     libre_init();
-    srtp_init();
-#if __APPLE__
-    apple_sound_init();
-#endif
 
     int err;
-    struct sip_addr *user_c;
-    struct uac *uac;
-    uac = mem_alloc(sizeof(struct uac), NULL);
+    struct tcsip *sip;
+    struct cli_app *app;
 
-    net_default_source_addr_get(AF_INET, &uac->laddr);
-    sa_set_port(&uac->laddr, 0);
+    app = mem_zalloc(sizeof(struct cli_app), NULL);
+    if(!app) {
+        err = -ENOMEM;
+        goto fail;
+    }
+    glob_app = app;// signal handlers suck
 
-    if(argc<2) {
-        printf("provide certificate path\n");
+    app->handlers = &cli_handlers;
+    app->handlers->arg = app;
+
+    tcsip_alloc(&app->sip, 0, app->handlers);
+    if(!app->sip) {
+        printf("failed to create driver instance\n");
         return 1;
     }
 
-    char *cname;
-    struct pl cname_p;
-    int after, before;
-    err = x509_info(argv[1], &after, &before, &cname);
-    if(err) {
-	fprintf(stderr, "can't load cert at %s\n", argv[1]);
-	return 1;
+    sip = app->sip;
+
+    if(argc<2) {
+        printf("provide login name\n");
+        return 1;
     }
-
-    pl_set_str(&cname_p, cname);
-    user_c = mem_zalloc(sizeof(*user_c), NULL);
-
-    err = sip_addr_decode(user_c, &cname_p);
-    if(err) return 1;
-
-    err = tls_alloc(&uac->tls, TLS_METHOD_SSLV23, argv[1], NULL);
-    tls_add_ca(uac->tls, "CA.cert");
-
-    err = dns_srv_get(NULL, 0, uac->nsv, &uac->nsc);
-
-    if(uac->nsc==0) {
-        uac->nsc = 1;
-        sa_set_str(uac->nsv, "8.8.8.8", 53);
-    }
-
-    err = dnsc_alloc(&uac->dnsc, NULL, uac->nsv, uac->nsc);
-
-
-    err = sip_alloc(&uac->sip, uac->dnsc, 32, 32, 32, 
-          USER_AGENT, exit_handler, NULL);
-
-    err = sip_transp_add(uac->sip, SIP_TRANSP_TLS, &uac->laddr,
-          uac->tls);
-
-    err = sipsess_listen(&uac->sock, uac->sip, 32, connect_handler, uac);
-
-
-    uac->local = user_c;
-
-    struct tcsipreg *sreg;
     struct pl instance_id;
     pl_set_str(&instance_id, "cc823c2297c211e28cd960c547067464");
-    tcsipreg_alloc(&sreg, uac);
-    tcop_users(sreg, user_c, user_c);
-    tcsreg_set_instance_pl(sreg, &instance_id);
-    tcsreg_handler(sreg, reg_change, uac);
-    tcsreg_state(sreg, 1);
+    tcsip_uuid(sip, &instance_id);
+
+    struct pl name;
+    pl_set_str(&name, argv[1]);
+
+    err = tcsip_local(sip, &name);
+    if(err) {
+        re_printf("local login failed  %d\n", err);
+        return err;
+    }
+
+    tcsip_set_online(sip, 1);
 
     if(argc>2) {
         exit_after = 1;
-        call(uac, argv[2]);
+        call(app, argv[2]);
     }
 
-    fd_listen(0, FD_READ, cmd_handler, uac);
+    fd_listen(0, FD_READ, cmd_handler, app);
 
     re_main(signal_handler);
 
-    mem_deref(uac->sip);
-    mem_deref(uac->dnsc);
+    mem_deref(sip);
 
+fail:
     libre_close();
     return 0;
 }
