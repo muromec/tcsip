c501bdb8fe5b674c19478d3b07d190bf1ad868c8
Ilya Petrov <ilya.muromec@gmail.com>
2013-12-18 02:04:19 +0700

    move sound macros to separate file

--- src/tcsip/tcmedia.c
+++ src/tcsip/tcmedia.c
@@ -9,36 +9,11 @@
 #include "tcsipcall.h"
 #include "tcmedia.h"
 
-#define O_LIM (320*12)
-
-#if __APPLE__
-#include "sound/apple/sound.h"
-#define sound_t apple_sound
-#define snd_open(__x) apple_sound_open(DIR_BI, 8000, O_LIM, __x)
-#define snd_start(__x) apple_sound_start(__x)
-#define snd_close(__x) {apple_sound_stop(__x);\
-	apple_sound_close(__x);}
-#endif
-#if __linux__ && !defined(ANDROID)
-#include "sound/linux/asound.h"
-#define sound_t alsa_sound
-#define snd_open(__x) alsa_sound_open(__x)
-#define snd_start(__x) alsa_sound_start(__x)
-#define snd_close(__x) alsa_sound_close(__x)
-#endif
-
-#if ANDROID
-#include "sound/android/opensl_io.h"
-#define sound_t opensl_sound
-#define snd_open(__x) opensl_sound_open(__x)
-#define snd_start(__x) opensl_sound_start(__x)
-#define snd_close(__x) opensl_sound_close(__x)
-#endif
+#include "sound/sound_macro.h"
 
 #define DEBUG_MODULE "tcmedia"
 #define DEBUG_LEVEL 5
 #include <re_dbg.h>
-
 
 
 struct tcmedia {
